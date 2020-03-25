a07188cef005c28e6fa5afa96f5ec5b9a4e74540
fl0rian <florian.buchacher@student.tugraz.at>
2020-03-25 12:09:18 +0100

    LANG-002 Added overview screen with listView [FB, KZ]

--- app/app/src/main/java/com/morning5/vocabularytrainer/MainActivity.java
+++ app/app/src/main/java/com/morning5/vocabularytrainer/MainActivity.java
@@ -2,13 +2,51 @@ package com.morning5.vocabularytrainer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.morning5.vocabularytrainer.database.DbHelper;
+import com.morning5.vocabularytrainer.dto.WordContract;
 
 public class MainActivity extends AppCompatActivity {
+
+    SQLiteDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button button_overview = findViewById(R.id.btn_overview);
+
+        button_overview.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, OverviewActivity.class);
+                MainActivity.this.startActivity(intent);
+            }
+        });
+
+        db = new DbHelper(getBaseContext()).getWritableDatabase();
+        addWords();
+    }
+
+    protected void addWords()
+    {
+        // Create a new map of values, where column names are the keys
+        ContentValues germanValues = new ContentValues();
+        germanValues.put(WordContract.Word.Word, "german");
+        germanValues.put(WordContract.Word.Language, "German");
+
+        ContentValues englishValues = new ContentValues();
+        englishValues.put(WordContract.Word.Word, "english");
+        englishValues.put(WordContract.Word.Language, "English");
+
+        // Insert the new row, returning the primary key value of the new row
+        long germanId = db.insert(WordContract.Word.TABLE_NAME, null, germanValues);
+        long englishId = db.insert(WordContract.Word.TABLE_NAME, null, englishValues);
     }
 }
--- app/app/src/main/java/com/morning5/vocabularytrainer/OverviewActivity.java
+++ app/app/src/main/java/com/morning5/vocabularytrainer/OverviewActivity.java
@@ -1,13 +1,73 @@
 package com.morning5.vocabularytrainer;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.morning5.vocabularytrainer.database.DbHelper;
+import com.morning5.vocabularytrainer.dto.TranslationContract;
+import com.morning5.vocabularytrainer.dto.WordContract;
+
+import java.util.ArrayList;
 
 public class OverviewActivity extends AppCompatActivity {
+
+    SQLiteDatabase sqLiteDatabase;
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_overview);
+
+        Intent intent = getIntent();
+
+        sqLiteDatabase = new DbHelper(getBaseContext()).getWritableDatabase();
+
+        listView = findViewById(R.id.list_view_overview);
+
+        showVocabularies();
+    }
+
+    private void showVocabularies() {
+
+        Cursor cursor = sqLiteDatabase.rawQuery("SELECT * FROM " + WordContract.Word.TABLE_NAME, null);
+
+        if (cursor.getCount() == 0) {
+            showMessage("Error", "Nothing found");
+        }
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        ArrayList<String> list = new ArrayList<String>();
+        ArrayAdapter adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, list);
+
+        while (cursor.moveToNext()) {
+            /*stringBuilder.append("Id :").append(cursor.getString(0)).append("\n");
+            stringBuilder.append("Word :").append(cursor.getString(1)).append("\n");
+            stringBuilder.append("Language :").append(cursor.getString(2)).append("\n");*/
+
+            list.add(cursor.getString(1));
+        }
+
+        //showMessage("Data", stringBuilder.toString());
+
+        cursor.close();
+
+        listView.setAdapter(adapter);
+    }
+
+    private void showMessage(String title, String message) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(message);
+        builder.show();
     }
 }
