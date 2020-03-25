f351ae345e26695a44deb47a9b412ad7c0dd7773
emreakca <40756776+emreakca@users.noreply.github.com>
2020-03-25 12:01:30 +0100

    001 Add New Vocabulary (#15)
    * LANG-001 Added New Page for add vocabulary
    Added Database [AY, MA, MV]
    
    * LANG-001 Updated the database [AY, MA, MV]
    
    * LANG-001 Added Testcase and Snackbar [AY, MA, MV]
    
    * LANG-001 Changed Layout [AY, MA, MV]
    
    * LANG-001 Changed Database [AY, MA, MV]

--- app/app/src/main/java/com/morning5/vocabularytrainer/MainActivity.java
+++ app/app/src/main/java/com/morning5/vocabularytrainer/MainActivity.java
@@ -1,8 +1,10 @@
 package com.morning5.vocabularytrainer;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onButtonClickSwitchToAddWord(View v) {
+        Intent addWordIntent = new Intent(getBaseContext(), AddWordActivity.class);
+        startActivity(addWordIntent);
     }
 }
---
+++ app/app/src/main/java/com/morning5/vocabularytrainer/dto/WordContract.java
@@ -0,0 +1,16 @@
+package com.morning5.vocabularytrainer.dto;
+
+import android.provider.BaseColumns;
+
+public final class WordContract {
+    private WordContract() {
+    }
+
+    public static class Word implements BaseColumns {
+        public static final String TABLE_NAME = "Word";
+        public static final String Word1 = "WordOne";
+        public static final String Language1 = "LanguageOne";
+        public static final String Word2 = "WordTwo";
+        public static final String Language2 = "LanguageTwo";
+    }
+}
