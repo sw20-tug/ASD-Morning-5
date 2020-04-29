9ada77eb0b524bc89cd065cf01bea47cfded386e
VedadMisirlic <62018039+VedadMisirlic@users.noreply.github.com>
2020-04-29 12:08:06 +0200

    LANG-012 Added translations to study interface [MV, AY] (#26)

--- app/app/src/androidTest/java/com/morning5/vocabularytrainer/BackupTest.java
+++ app/app/src/androidTest/java/com/morning5/vocabularytrainer/BackupTest.java
@@ -12,10 +12,7 @@ import org.junit.runner.RunWith;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.action.ViewActions.replaceText;
-import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.assertEquals;
 
 /**
