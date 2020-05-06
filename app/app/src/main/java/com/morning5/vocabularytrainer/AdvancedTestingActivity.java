14d491d859eace5c675a41b2419d4a472c371698
Emre <yunus.akca@student.tugraz.at>
2020-05-06 09:05:55 +0200

    [LANG-008] Added Advanced testing files [AY, KP]

---
+++ app/app/src/main/java/com/morning5/vocabularytrainer/AdvancedTestingActivity.java
@@ -0,0 +1,25 @@
+package com.morning5.vocabularytrainer;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AdvancedTestingActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener  {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.);
+
+    }
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+}
